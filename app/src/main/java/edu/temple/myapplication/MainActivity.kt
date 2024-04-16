c01d24963cc795d85f89cb0d70a447bbdcc2d99e
chris-douglas13 <111987005+chris-douglas13@users.noreply.github.com>
2024-04-16 12:01:41 -0400

    added menu functionality

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -13,19 +15,17 @@ class MainActivity : AppCompatActivity() {
     lateinit var timerBinder: TimerService.TimerBinder
     var isConnected = false
 
-    val serviceConnection = object : ServiceConnection{
+    private val connection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            timerBinder = service as TimerService.TimerBinder
+            val binder = service as TimerService.TimerBinder
+            timerBinder = binder
             isConnected = true
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
             isConnected = false
         }
-
     }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,21 +33,51 @@ class MainActivity : AppCompatActivity() {
 
         bindService(
             Intent(this, TimerService::class.java),
-            serviceConnection,
-            BIND_AUTO_CREATE)
+            connection,
+            BIND_AUTO_CREATE
+        )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-            if(isConnected) timerBinder.start(100)
+            if (isConnected) timerBinder.start(10)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            if(isConnected) timerBinder.pause()
-
+            if (isConnected) timerBinder.pause()
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
-            if(isConnected) timerBinder.stop()
 
+        findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if (isConnected) timerBinder.stop()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.startOption -> {
+                if (isConnected) timerBinder.start(10)
+            }
+
+            R.id.pauseOption -> {
+                if (isConnected) timerBinder.pause()
+            }
+
+            R.id.stopOption -> {
+                if (isConnected) timerBinder.stop()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isConnected) {
+            unbindService(connection)
+            isConnected = false
         }
     }
 }
